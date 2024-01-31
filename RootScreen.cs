ff4e9e09ebbe0d258f66b0a4a2801caccda0b172
Alexej <h23alto@axevalla.fhsk.se>
2024-01-31 11:58:36 +0100

    Spel Scen

---
+++ RootScreen.cs
@@ -0,0 +1,12 @@
+﻿internal class RootScreen : ScreenObject
+{
+    private ScreenSurface _map;
+
+    public RootScreen()
+    {
+        _map = new ScreenSurface(Game.Instance.ScreenCellsX, Game.Instance.ScreenCellsY - 5);
+        _map.UseMouse = false;
+
+        Children.Add(_map);
+    }
+}
